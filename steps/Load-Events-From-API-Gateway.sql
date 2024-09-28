cc47c7f4fe9039b821bda01a98b2f388f3acd01f
Ella Pham <thanhthuyphamvn@gmail.com>
2024-09-28 09:38:58 +0000

    Load events

---
+++ steps/Load-Events-From-API-Gateway.sql
@@ -0,0 +1,42 @@
+--Create external function to call Lambda function Get Inflation Rate:
+CREATE OR REPLACE EXTERNAL FUNCTION DATAPROJECT_{{environment}}.PUBLIC.GET_EVENT
+    RETURNS VARIANT
+    API_INTEGRATION = get_event_api
+    AS 'https://2wwl89e43c.execute-api.eu-west-2.amazonaws.com/Test/Get_Inflation_Rate';
+
+//Create raw event table
+CREATE OR REPLACE TABLE DATAPROJECT_{{environment}}.RAW."trade_events" (
+    "dateUtc" TIMESTAMP_LTZ
+    ,"isBetterThanExpected" STRING
+    ,"name" STRING
+    ,"volatility" STRING
+);
+
+//Create procedure to call API, then copy result file stored in S3 into table
+CREATE OR REPLACE PROCEDURE DATAPROJECT_{{environment}}.PUBLIC.Load_Event()
+RETURNS VARCHAR
+LANGUAGE SQL
+AS
+BEGIN
+   SELECT DATAPROJECT_{{environment}}.PUBLIC.GET_EVENT();
+   LET sql := 'COPY INTO DATAPROJECT_{{environment}}.RAW."trade_events" 
+                FROM (
+                    SELECT $1:dateUtc::TIMESTAMP_LTZ
+                        , $1:isBetterThanExpected::VARCHAR
+                        , $1:name::VARCHAR
+                        , $1:volatility::VARCHAR
+                    FROM @DATAPROJECT_{{environment}}."PUBLIC"."S3_STAGE_EVENTS"/<date>/trade_event.json
+                )
+                FILE_FORMAT = DATAPROJECT_{{environment}}.PUBLIC.MY_JSON_FORMAT
+                ON_ERROR=ABORT_STATEMENT;';
+   sql := REPLACE(sql, '<date>', TO_VARCHAR(CURRENT_DATE(), 'YYYYMMDD'));
+   EXECUTE IMMEDIATE (sql);
+   RETURN sql;
+END;
+
+
+//Create Task to Call stored procedures to call API to load data
+CREATE OR REPLACE TASK DATAPROJECT_{{environment}}.PUBLIC.LOAD_EVENT
+WAREHOUSE = COMPUTE_WH
+AFTER   DATAPROJECT_{{environment}}.PUBLIC.LOAD_EMPLOYMENT_BY_INDUSTRY
+AS CALL DATAPROJECT_{{environment}}.PUBLIC.LOAD_EVENT();
