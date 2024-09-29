4e63fe0c17cc4370c72732ad6ab359e8df5960a7
Ella Pham <thanhthuyphamvn@gmail.com>
2024-09-29 17:08:41 +0000

    Change load condition

--- deploy_pipeline.sql
+++ deploy_pipeline.sql
@@ -1,7 +1,7 @@
 execute immediate from 'steps/DB-Setup.sql' using (environment => '{{environment}}', lookerpassword => '{{lookerpassword}}');;
 execute immediate from 'steps/Load-Raw-Tables.sql' using (environment => '{{environment}}');;
-execute immediate from 'steps/Load-Events-From-API-Gateway.sql' using (environment => '{{environment}}');;
 execute immediate from 'steps/Create-DW-Tables.sql' using (environment => '{{environment}}');;
 execute immediate from 'steps/Load-DW-Main-Tables.sql' using (environment => '{{environment}}');;
 execute immediate from 'steps/Load-DW-Hierachy-Tables.sql' using (environment => '{{environment}}');;
+execute immediate from 'steps/Load-Events-From-API-Gateway.sql' using (environment => '{{environment}}');;
 execute immediate from 'steps/Load-Serve-Tables.sql' using (environment => '{{environment}}');;
