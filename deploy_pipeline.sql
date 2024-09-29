4db344f9a73258fe6817125989e0ab366220144f
Ella Pham <thanhthuyphamvn@gmail.com>
2024-09-29 17:29:02 +0000

    Add step to resume all tasks

--- deploy_pipeline.sql
+++ deploy_pipeline.sql
@@ -1,9 +1,9 @@
 execute immediate from 'steps/DB-Setup.sql' using (environment => '{{environment}}', lookerpassword => '{{lookerpassword}}');;
-SELECT 'Load raw tables completed.';
 execute immediate from 'steps/Load-Raw-Tables.sql' using (environment => '{{environment}}');;
-
 execute immediate from 'steps/Create-DW-Tables.sql' using (environment => '{{environment}}');;
 execute immediate from 'steps/Load-DW-Main-Tables.sql' using (environment => '{{environment}}');;
 execute immediate from 'steps/Load-DW-Hierachy-Tables.sql' using (environment => '{{environment}}');;
 execute immediate from 'steps/Load-Events-From-API-Gateway.sql' using (environment => '{{environment}}');;
 execute immediate from 'steps/Load-Serve-Tables.sql' using (environment => '{{environment}}');;
+execute immediate from 'steps/Resume-All-Tasks.sql' using (environment => '{{environment}}');;
+
